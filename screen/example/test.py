e221c556c29260712bbae78a8486d674fd26cac5
Weiyu <wwang1110@gmail.com>
2023-12-14 17:39:16 -0800

    add ipynb

--- screen/example/test.py
+++ screen/example/test.py
@@ -1,14 +1,10 @@
 import torch
 from peft import PeftModel
-
 from example.mlp import MLP
-from example.helper import print_model_info
 
 base_model = MLP.from_pretrained("./base")
-
 model = PeftModel.from_pretrained(base_model, './lora1', adapter_name="adapter1")
 model.load_adapter('./lora2', adapter_name="adapter2")
-
 
 model.set_adapter("adapter1")
 inputs1 = {'input_ids' : torch.rand(2, 20).float()}
